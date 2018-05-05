af853fa674bdab9e3f0c816338cf4a2527fb9b54
Dominoidas <antihumanistas@gmail.com>
2018-05-05 03:42:28 +0300

    finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,36 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+	#def initialize
+	#	sp
+	#end
 	def sp
-	s = ['S', 'Z', 'C', 'S']
-	t = ['t', 'zt', 'ct', 'st']
-	a = ['a', 'å', 'ä', 'ą']
-	l = ['l', 'll', 'л', 'лл', 'lъ', 'llъ', 'лъ', 'ллъ',]
-	c = ['c', 'z', 's', 'c', 'z', 'ц']
-	i = ['i', '', 'и']
-	ja = ['ja', 'ya', 'ia', 'иa','я', 'ѧ']
-	space = [' ', ' ', ' ', '-', '_', '*', '|', '||']
-	pre = ['Pre', 'Пre', 'Ψre', 'Ξre', 'Pрe', 'Прe', 'Ψрe', 'Ξрe',
-				'Prϵ', 'Пrϵ', 'Ψrϵ', 'Ξrϵ', 'Pрϵ', 'Прϵ', 'Ψрϵ', 'Ξрϵ',
-				'Prε', 'Пrε', 'Ψrε', 'Ξrε', 'Pрε', 'Прε', 'Ψрε', 'Ξрε']
-	fi = ['f', 'ff', 'ф', 'φ', 'ѳ', 'þ', 'ξ']
-	jota = ['i', 'и', 'ι', 'ї', 'ј']
-	is = ['s', 'z', 'c', 'σ', 'ς',
-			'ss', 'sz', 'sσ', 'sς', 'zs', 'cs', 'σs',
-			'zz', 'zc', 'zσ', 'zς', 'cz', 'σz',
-			'cc', 'cσ', 'cς','sc', 'σc',
-			'σσ', 'σς', 'ςς',
-			'ssz', 'ssσ', 'ssς', 'zss', 'css', 'σss',
-			'scz']
-	sparray = [s, t, a, l, c, i, ja, space, pre, fi, jota, is]
-	cilibles = []
-	for i in sparray
-		cilibles.push(i[rand(i.length)])
-	end
-	@spreffis = cilibles.join
+		# --Stallция Preφιcς--
+		s = ['S', 'Z', 'C', 'S']
+		t = ['t', 'zt', 'ct', 'st']
+		a = ['a', 'å', 'ä', 'ą']
+		l = ['l', 'll', 'л', 'лл', 'lъ', 'llъ', 'лъ', 'ллъ',]
+		c = ['c', 'z', 's', 'c', 'z', 'ц']
+		i = ['i', '', 'и']
+		ja = ['a', 'ja', 'ya', 'ia', 'иa','я', 'ѧ']
+		space = [' ', ' ', ' ', '-', '_', '*', '|', '||']
+		pp = ['P', 'П', 'Ψ', 'Ξ']
+		r = ['r', 'р']
+		e = ['e', 'ϵ', 'ε']
+		fi = ['f', 'ff', 'ф', 'φ', 'ѳ', 'þ', 'ξ']
+		jota = ['i', 'и', 'ι', 'ї', 'ј']
+		is = ['s', 'z', 'c', 'σ', 'ς',
+				'ss', 'sz', 'sσ', 'sς', 'zs', 'cs', 'σs',
+				'zz', 'zc', 'zσ', 'zς', 'cz', 'σz',
+				'cc', 'cσ', 'cς', 'sc', 'σc',
+				'σσ', 'σς', 'ςς',
+				'ssz', 'ssσ', 'ssς', 'zss', 'css', 'σss',
+				'scz']
+		sparray = [s, t, a, l, c, i, ja, space, pp, r, e, fi, jota, is]
+		cilibles = []
+		for i in sparray
+			cilibles.push(i[rand(i.length)])
+		end
+		@spreffis = cilibles.join
 	end
 	def op
 		sp
--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-	has_many :microposts
-	validates :email, presence: true
-	validates :name, length: {maximum: 32}, presence: true
-end
